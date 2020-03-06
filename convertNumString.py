9e3bbac5d4aa888890a8feb09c48032f99ab29a2
Kyle L <likyle001@gmail.com>
2020-03-06 17:11:54 -0600

    finished string to number converter

---
+++ convertNumString.py
@@ -0,0 +1,30 @@
+def convertString (input_string):
+    output = 0
+    input_string = input_string.strip()
+    neg = 0
+
+    for i in range(0, len(input_string)):
+        output *= 10
+        current = ord(input_string[i])
+        if(current == 45 and len(input_string) > 1 and i == 0):
+            neg = 1
+        elif(current > 47 and current < 58):
+            output += current - 48
+        else:
+            return None
+    
+    if(neg):
+        output *= -1
+    
+    return output
+
+
+
+
+# first line
+a = "-10924083"
+result = convertString(a)
+
+f = open("convertNumString_Output.txt","w+")
+f.write(str(result))
+f.close
